e931aa171e7a8815f27aa8f40b013c68c705bff1
Xander Houtman <xander.houtman@yacht.nl>
2018-04-16 10:43:56 +0200

    Added most of the views from the mock up

---
+++ WebInterface/Controllers/MenuController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebInterface.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Drinks()
+        {
+            return View();
+        }
+
+        public IActionResult Starters()
+        {
+            return View();
+        }
+
+        public IActionResult Mains()
+        {
+            return View();
+        }
+
+        public IActionResult Desserts()
+        {
+            return View();
+        }
+
+        public IActionResult OrderOverview()
+        {
+            return View();
+        }
+    }
+}
