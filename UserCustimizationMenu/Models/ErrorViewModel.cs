143b1be08d32013f7800921687998eb70b28b2b2
kirstentuut <random@hotmail.com>
2018-04-18 14:58:14 +0200

    Start with the customization page

---
+++ UserCustimizationMenu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserCustimizationMenu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UserCustimizationMenu/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace UserCustimizationMenu.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string UserPasword { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
