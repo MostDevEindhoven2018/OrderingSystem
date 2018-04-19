8fe96e2ffcd644ce3b36c3b6ea9065050ac9b1fa
SupremeNewb <royvanstralen@hotmail.com>
2018-04-19 14:34:24 +0200

    Removed Empty Ordering system

--- UserCustimizationMenu/Controllers/UserController.cs
+++ UserCustimizationMenu/Controllers/UserController.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using UserCustimizationMenu.Controllers;
-//using UserCustimizationMenu.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserCustimizationMenu.Controllers;
+using UserCustimizationMenu.Models;
 
-//namespace UserCustimizationMenu.Controllers
-//{
-//    public class UserController : Controller
-//    {
-//        public UserController(MyDbContext _context)
-//        {
-//            ctx = _context;
-//        }
+namespace UserCustimizationMenu.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController(MyDbContext _context)
+        {
+            ctx = _context;
+        }
 
-//        MyDbContext ctx = null;
+        MyDbContext ctx = null;
 
-//        public IActionResult Login()
-//        {
+        public IActionResult Login()
+        {
 
-//            return View();
-//        }
-//    }
-//}
+            return View();
+        }
+    }
+}
