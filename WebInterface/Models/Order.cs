6bf1c58798807a98ccb2a14d3164cc652673390b
TUMMALAPALLI1993 <tummalapalli.kirankumar@gmail.com>
2018-04-25 09:07:22 +0200

    minor changes

--- WebInterface/Models/Order.cs
+++ WebInterface/Models/Order.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace WebInterface.Models
 {
     public class Order
     {
+        [Key]
         public int OrderID { get; set; }
         /// <summary>
         /// Dishes added to the Order, but not send to the kitchen
         /// </summary>
+
+
+   
+        public int quantity {get;set;}
+
         public virtual ICollection<Dish> Selected { get; set; }
 
         /// <summary>
