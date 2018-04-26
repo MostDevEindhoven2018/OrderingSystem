5e4449571ad7430a2ef357fc93ebcc387ddc85fd
sangre5 <pjuradosolis@gmail.com>
2018-04-26 13:21:09 +0200

    combining views

---
+++ WebInterface/Migrations/20180425131537_TablesInfo.cs
@@ -0,0 +1,285 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebInterface.Migrations
+{
+    public partial class TablesInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DishTypes",
+                columns: table => new
+                {
+                    DishTypeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    SubDishTypeID = table.Column<int>(nullable: false),
+                    SubType = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishTypes", x => x.DishTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientTypes",
+                columns: table => new
+                {
+                    IngredientTypeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    UnitPlural = table.Column<string>(nullable: true),
+                    UnitSingular = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientTypes", x => x.IngredientTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tables",
+                columns: table => new
+                {
+                    TableID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Capacity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tables", x => x.TableID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HidePrice = table.Column<bool>(nullable: false),
+                    TableID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupID);
+                    table.ForeignKey(
+                        name: "FK_Groups_Tables_TableID",
+                        column: x => x.TableID,
+                        principalTable: "Tables",
+                        principalColumn: "TableID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    GuestID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Code = table.Column<string>(maxLength: 128, nullable: true),
+                    GroupID = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.GuestID);
+                    table.ForeignKey(
+                        name: "FK_Guests_Groups_GroupID",
+                        column: x => x.GroupID,
+                        principalTable: "Groups",
+                        principalColumn: "GroupID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    BillID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PayerGuestID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.BillID);
+                    table.ForeignKey(
+                        name: "FK_Bills_Guests_PayerGuestID",
+                        column: x => x.PayerGuestID,
+                        principalTable: "Guests",
+                        principalColumn: "GuestID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OwnerGuestID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Guests_OwnerGuestID",
+                        column: x => x.OwnerGuestID,
+                        principalTable: "Guests",
+                        principalColumn: "GuestID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    DishID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrderID = table.Column<int>(nullable: true),
+                    OrderID1 = table.Column<int>(nullable: true),
+                    TypeDishTypeID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.DishID);
+                    table.ForeignKey(
+                        name: "FK_Dishes_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Dishes_Orders_OrderID1",
+                        column: x => x.OrderID1,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Dishes_DishTypes_TypeDishTypeID",
+                        column: x => x.TypeDishTypeID,
+                        principalTable: "DishTypes",
+                        principalColumn: "DishTypeID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    IngredientID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DishID = table.Column<int>(nullable: true),
+                    DishTypeID = table.Column<int>(nullable: true),
+                    Quantity = table.Column<double>(nullable: false),
+                    TypeIngredientTypeID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.IngredientID);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_Dishes_DishID",
+                        column: x => x.DishID,
+                        principalTable: "Dishes",
+                        principalColumn: "DishID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_DishTypes_DishTypeID",
+                        column: x => x.DishTypeID,
+                        principalTable: "DishTypes",
+                        principalColumn: "DishTypeID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Ingredients_IngredientTypes_TypeIngredientTypeID",
+                        column: x => x.TypeIngredientTypeID,
+                        principalTable: "IngredientTypes",
+                        principalColumn: "IngredientTypeID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_PayerGuestID",
+                table: "Bills",
+                column: "PayerGuestID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_OrderID",
+                table: "Dishes",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_OrderID1",
+                table: "Dishes",
+                column: "OrderID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_TypeDishTypeID",
+                table: "Dishes",
+                column: "TypeDishTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_TableID",
+                table: "Groups",
+                column: "TableID",
+                unique: true,
+                filter: "[TableID] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_GroupID",
+                table: "Guests",
+                column: "GroupID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_DishID",
+                table: "Ingredients",
+                column: "DishID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_DishTypeID",
+                table: "Ingredients",
+                column: "DishTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_TypeIngredientTypeID",
+                table: "Ingredients",
+                column: "TypeIngredientTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OwnerGuestID",
+                table: "Orders",
+                column: "OwnerGuestID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "IngredientTypes");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "DishTypes");
+
+            migrationBuilder.DropTable(
+                name: "Guests");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Tables");
+        }
+    }
+}
