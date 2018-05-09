897f9889c7c418485de8ea5d623b9318c4b7af7c
sangre5 <pjuradosolis@gmail.com>
2018-05-09 15:47:08 +0200

    que

--- Dummy menu data.sql
+++ Dummy menu data.sql
@@ -9,6 +9,7 @@ insert into SubDishTypes(SubType) values ('Sub-Mains') --7
 insert into SubDishTypes(SubType) values ('Sub-Starters') --8
 
 --Drinks demo
+<<<<<<< HEAD
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (1,1,'Cola',2,'aaa')
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (1,1,'Fanta',2,'aaa')
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (1,2,'Tea',2,'aaa')
@@ -38,3 +39,34 @@ insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (4,5,'Banan
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (4,5,'Fruit salad',10,'aaa')
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (4,6,'Apple pie',8,'aaa')
 insert into DishTypes(Course,SubDishTypeID,Name,Price,Recipe) values (4,6,'Chocolate pie',10,'aaa')
+=======
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,1,'Cola',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,1,'Fanta',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,2,'Tea',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,2,'Coffee',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,3,'Red Wine',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (1,3,'Beer',10,'mu')
+
+--Starters demo
+insert into DishTypes(Course,Name, Price, Recipe) values (2,'Carpachio',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (2,'Tuna Salad',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (2,'Shrimps',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (2,'Tomato Soup',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (2,'Onion Soup',10,'mu')
+
+--Mains demo
+insert into DishTypes(Course,Name, Price, Recipe) values (3,'Steak',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (3,'Salmon',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (3,'Chicken breast',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (3,'Duck',10,'mu')
+insert into DishTypes(Course,Name, Price, Recipe) values (3,'Vega burger',10,'mu')
+
+--Dessert demo
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,4,'Vanilla Ice Cream',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,4,'Straberry Ice Cream',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,4,'Pear Ice Cream',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,5,'Banana split',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,5,'Fruit salad',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,6,'Apple pie',10,'mu')
+insert into DishTypes(Course,SubDishTypeID,Name, Price, Recipe) values (4,6,'Chocolate pie',10,'mu')
+>>>>>>> que
