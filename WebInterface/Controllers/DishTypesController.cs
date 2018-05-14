869f8ce2ed0590df7c71420a6ae5d11b82e4dbba
SupremeNewb <royvanstralen@hotmail.com>
2018-05-14 17:27:01 +0200

    Moved database functions to repository. fixed datatables for dishtypes removed sort/search for edit column datatables

--- WebInterface/Controllers/DishTypesController.cs
+++ WebInterface/Controllers/DishTypesController.cs
@@ -85,7 +85,7 @@ namespace WebInterface.Controllers
         }
 
         // Remove ingredients from the dish
-        public IActionResult RemoveIngredient(int? DishTypeID, int? IngredientID)
+        public async Task<IActionResult> RemoveIngredient(int? DishTypeID, int? IngredientID)
         {
             if (IngredientID == null)
             {
@@ -97,21 +97,17 @@ namespace WebInterface.Controllers
                 return NotFound();
             }
 
-            repo.RemoveIngredientID(IngredientID);
-            repo.Save();
+            repo.RemoveIngredient(IngredientID);
+            await repo.Save();
 
             return RedirectToAction("Edit", new { id = DishTypeID });
         }
 
-        public IActionResult SaveQuantity (int DishTypeID, int IngredientID, int Quantity)
+        public async Task<IActionResult> SaveQuantity (int DishTypeID, int IngredientTypeID, int Quantity)
         {
-            
-            var ingredientQuery = _context.Ingredients.Where(x => x.IngredientID == IngredientID);
 
-            Ingredient ingredient = ingredientQuery.FirstOrDefault();
-
-            ingredient.Quantity = Quantity;
-            _context.SaveChanges();
+            await repo.GetIngredientTypeID(IngredientTypeID, Quantity);
+            await repo.Save();
 
             return RedirectToAction("Edit", new { id = DishTypeID });
         }
@@ -137,8 +133,6 @@ namespace WebInterface.Controllers
         // GET: DishTypes/Create
         public async Task<IActionResult> Create()
         {
-            repo.EnsureCreated();
-            
             DishTypesViewModel model = new DishTypesViewModel
             {
                 SubTypeList = await repo.GetSubDishTypesList()
@@ -166,7 +160,7 @@ namespace WebInterface.Controllers
                     Price = dishTypeViewModel.Dish.Price
                 };
 
-                var sdt = repo.GetSubDishTypeID(dishTypeViewModel);               
+                var sdt = await repo.GetSubDishTypeID(dishTypeViewModel);               
 
 
                 if (sdt == null)
